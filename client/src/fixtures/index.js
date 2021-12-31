c3417d3c04e914f6bb8bbfe4c93b303b20af38f9
Mohamed <daadaamed@gmail.com>
2021-12-31 15:23:48 +0100

    Part 1 Complete

---
+++ client/src/fixtures/index.js
@@ -0,0 +1,19 @@
+export const singularStory = {
+  by: 'Karl Hadwen',
+  id: 1,
+  time: 1567209822,
+  title: 'Tarnished: Google Responds',
+  url: 'https://something.com/hackernewstut',
+  score: 4
+};
+
+export const storyIds = [1];
+
+export const emptySingularStory = {
+  by: undefined,
+  id: undefined,
+  time: undefined,
+  title: undefined,
+  url: undefined,
+  score: undefined
+};
